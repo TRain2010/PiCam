9b72268281b559c48d6f55a898bb5f9782380dc2
TRain2010 <tianyu.liang@big6media.com>
2021-03-10 01:49:50 -0600

    update stream

--- obj_detect.py
+++ obj_detect.py
@@ -34,14 +34,14 @@ def getObjects(img,thres,nms,draw=True,objects=[]):
     return img,objectInfo
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(3, 640)
     cap.set(4, 480)
     # cap.set(10,70)
 
     while True:
         success, img = cap.read()
-        result,objectInfo = getObjects(img,0.45,0.2,objects=[‘cup’,’mouse’])
+        result,objectInfo = getObjects(img,0.45,0.2,objects=[])
         #print(objectInfo)
         cv2.imshow("Output", img)
         cv2.waitKey(1)
