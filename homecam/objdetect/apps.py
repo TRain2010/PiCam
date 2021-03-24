dc4037e7afc67cdf94dcc7fe6b4388f0e9c66560
TRain2010 <tianyu.liang@big6media.com>
2021-03-24 04:45:25 -0500

    update

---
+++ homecam/objdetect/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ObjdetectConfig(AppConfig):
+    name = 'objdetect'
--- homecam/streaming/views.py
+++ homecam/streaming/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators import gzip
 from django.http import StreamingHttpResponse, HttpResponse
 from .models import VideoFeeds
+from objdetect.obj_detect import getObjects
 
 import socket
 import sys
@@ -38,12 +39,12 @@ def generate():
         frame_data = data[:msg_size]
         data = data[msg_size:]
         frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-        #result,objectInfo = getObjects(frame,0.45,0.2,objects=[])
         frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        result,objectInfo = getObjects(frame,0.45,0.2,objects=[])
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        _, result = cv2.imencode('.jpg', result, encode_param)
         yield(b'--frame\r\n'
-              b'Content-Type: image/jpg\r\n\r\n' + frame.tobytes() + b'\r\n\r\n')
+              b'Content-Type: image/jpg\r\n\r\n' + result.tobytes() + b'\r\n\r\n')
 
 @gzip.gzip_page
 def live_feed(reqeust):
